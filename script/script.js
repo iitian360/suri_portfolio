5f211dce1f2c93cc774673c47e0c91e1b2d3c8e7
suri <sp48840@gmail.com>
2022-02-06 08:53:30 +0530

    this is a temperary change in nav

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//  var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-350px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+ var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-350px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
 
