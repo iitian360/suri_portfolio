ef4c5425aa6b6c707f72551ec12bda2c368e3a7c
suri <sp48840@gmail.com>
2022-03-16 09:44:34 +0530

    broad change

--- script/script.js
+++ script/script.js
@@ -1,15 +1,16 @@
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
- var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-350px";
-  }
-  prevScrollpos = currentScrollPos;
-}
 
+
+
+
+
+const burger=document.querySelector('.burger');
+let container=document.querySelector('.container')
+// console.log(burger);
+
+burger.addEventListener('click',()=>{
+  container.classList.toggle('active');
+})
 
 
 // document.getElementById("html").addEventListener("click",function(){
@@ -22,47 +23,16 @@ window.onscroll = function() {
 
 
 
-burger=document.querySelector('.burger');
-navlist=document.querySelector('.nav_list');
-searchbox=document.querySelector('.search_box');
-navbar=document.querySelector('.backgroundb');
-burger.addEventListener("click",function(){
-  navlist.classList.toggle('v-resp');
-  searchbox.classList.toggle('v-resp');
-  navbar.classList.toggle('h-nav-resp');
+// about hide text//
+
+const hide__text=document.querySelector('.hide__text');
+let abt__btn=document.getElementById('btn__abt');
+abt__btn.addEventListener('click',()=>{
+  hide__text.classList.toggle('hide');
+
+
 })
 
 
-let html_btn=document.getElementById('html');
-html_btn.addEventListener('click', getcertificate);
+  
 
-function getcertificate(){
-  const xhr= new XMLHttpRequest();
-
-  xhr.open('GET','./image/suraj.txt', true);
- xhr.send();
-  xhr.onload=function(){
-  let img_box=document.getElementById('certificate-1');
-  console.log(img_box);
-  img_box.innerHTML=`${this.responseText}`;
-  }
- 
-}
-
-let searchtxt=document.getElementById('search');
-searchtxt.addEventListener('input',function(){
-    let inputval=searchtxt.value.toLowerCase();
-    notecard=document.getElementsByClassName('suraj');
-    Array.from(notecard).forEach(function(element){
-        let cardtxt=element.getElementsByTagName("p")[0].innerText;
-        
-        let divtxt=element.getElementsByTagName("div")[0].innerText;
-        if (divtxt.includes(inputval)||cardtxt.includes(inputval)){
-          element.style.display="block";  
-        }
-
-        else{
-            element.style.display="none";
-        }
-    })
-})
